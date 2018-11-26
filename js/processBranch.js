a4bc5144ca482be914c342e89b393934618b7465
jiaweitan <gamilsphat@gmail.com>
2018-11-25 16:25:37 -0800

    change the size of the branch circle based on their # of commits

--- js/processBranch.js
+++ js/processBranch.js
@@ -4,14 +4,19 @@ var originals;
 var numberOfBranches;
 var workingBranches;
 var branchCommits = [];
-var map = new Object;
-//var map = new Hashmap();
+var shaName;
+var map;
+var largerCommits;
+
 // get the same files that different branches are working on
 function getBranchesSameFiles() {
     filenamesDataset = [];
     originals = [];
     numberOfBranches = 0;
     workingBranches = [];
+    map = {};
+    shaName = {};
+    largerCommits = 80;
     $.get("https://api.github.com/repos/" + searchterm + "/" + repoterm + "/branches",
         function (data, status) {
             console.log(status);
@@ -19,10 +24,12 @@ function getBranchesSameFiles() {
             if (data.length > 1) {
                 for (var i in data) {
                     if (data[i].name !== master) {
-                        originals.push(data[i].name);
-                        branchCommits.push(data[i]);
-
-                                }
+                        var name = data[i].name;
+                        var sha = data[i]["commit"]["sha"];
+                        originals.push(name);
+                        branchCommits.push(sha);
+                        shaName[sha] = name;
+                    }
                 }
                 numberOfBranches = originals.length;
                 getBranchFiles(getFilenames);
@@ -31,36 +38,6 @@ function getBranchesSameFiles() {
             }
         });
 }
-
-function getCommitsPerBranch() {
-    for (var i=0; i < branchCommits.length; i++){
-        var name = branchCommits[i].name;
-        var size = 0;
-    $.get("https://api.github.com/repos/" + repoterm + "/commits?per_page=100&sha=" + branchCommits[i].sha,
-    function (data, status) {
-        console.log(status);
-        size = data.length + 1;
-        map[name]=size;
-
-            });
-    } }
-
-
-//
-// function getWorkingBranches(data) {
-//     var master = "master";
-//     if (data.length > 1) {
-//         for (var i in data) {
-//             if (data[i].name !== master) {
-//                 originals.push(data[i].name);
-//             }
-//         }
-//         numberOfBranches = originals.length;
-//         getBranchFiles(getFilenames)
-//     } else {
-//         alert("no same working files");
-//     }
-// }
 
 // store filenames and branch as object {branch:[filenames]}
 function getFilenames(data) {
@@ -94,15 +71,25 @@ function getBranchFiles(callback) {
 function updateChartForSameFiles(data) {
     var dataset = preProcessFileData(data);
     var branches = workingBranches;
-    getCommitsPerBranch();
-    // makeCircles(Object.keys(dataset), (chartWidth)/(Object.keys(dataset).length + 1), chartHeight/2);
-    // makeCircles(branches, (chartWidth)/(branches.length + 1), chartHeight * 3 /4);
-    // makeLines2(dataset);
-    if (branches.length > 0) {
-        var nodes_links = makeDataset(dataset, branches);
-        drawGraph(nodes_links);
-    } else {
-        alert("All branches are merged!")
+
+    while (branchCommits.length > 0) {
+        $.get("https://api.github.com/repos/" + searchterm + "/" + repoterm + "/commits?per_page=100&sha=" + branchCommits.pop(),
+            function (data, status) {
+                console.log(status);
+                var sha = data[0]["sha"];
+                var name = shaName[sha];
+                map[name] = data.length + 1;
+                if (data.length + 1 > largerCommits)
+                    largerCommits = data.length + 1;
+                if (Object.keys(map).length === numberOfBranches) {
+                    if (branches.length > 0) {
+                        var nodes_links = makeDataset(dataset, branches);
+                        drawGraph(nodes_links);
+                    } else {
+                        alert("All branches are merged!")
+                    }
+                }
+            });
     }
 }
 
@@ -126,26 +113,6 @@ function preProcessFileData(data) {
         }
     }
     return dataset;
-}
-
-// Lines to connect branch and files
-function makeLines2(dataset) {
-    for (var i in dataset) {
-        var branches = dataset[i];
-        var file_cx = Number(document.getElementById(i).getAttribute("cx"));
-        var file_cy = Number(document.getElementById(i).getAttribute("cy"));
-        for (var j in branches) {
-            var br = branches[j];
-            var br_cx = Number(document.getElementById(br).getAttribute("cx"));
-            var br_cy = Number(document.getElementById(br).getAttribute("cy"));
-            svg.append("line")
-                .style("stroke", "black")
-                .attr("x1", file_cx)
-                .attr("y1", file_cy)
-                .attr("x2", br_cx)
-                .attr("y2", br_cy);
-        }
-    }
 }
 
 function makeDataset(dataset, branches) {
@@ -188,10 +155,9 @@ function drawGraph(dataset) {
     var link_data = dataset[1];
     var radius = 5;
 
-
     simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d){ return d.id;}).distance(80))
-        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink().id(function(d){ return d.id;}).distance(largerCommits))
+        .force("charge", d3.forceManyBody().strength(-largerCommits))
         .force("center", d3.forceCenter(chartWidth / 2, chartHeight / 2));
 
 
@@ -208,7 +174,12 @@ function drawGraph(dataset) {
         .data(node_data)
         .enter().append("circle")
         .attr("id", function (d) { return d.id})
-        .attr("r", function(d){return map[d.id];})
+        .attr("r", function(d){
+            if (map.hasOwnProperty(d.id))
+                return map[d.id];
+            else
+                return 5;
+        })
         .attr("fill", circleColor)
         .attr("onclick", "testClick(this.id)")
         .call(d3.drag()
@@ -235,44 +206,6 @@ function drawGraph(dataset) {
             .attr("cx", function (d) { return d.x = Math.max(radius, Math.min(chartWidth - radius, d.x))})
             .attr("cy", function (d) { return d.y = Math.max(radius, Math.min(chartHeight - radius, d.y))});
     }
-
-    // TODO: OLD
-    // var simulation = d3.forceSimulation().nodes(node_data);
-    // simulation
-    //     .force("charge_force", d3.forceManyBody())
-    //     .force("center_force", d3.forceCenter(width/2, height/2));
-    // var node = svg.append("g")
-    //     .attr("class", "nodes")
-    //     .selectAll("circle")
-    //     .data(node_data)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("r", 5)
-    //     .attr("fill", "red");
-    //
-    // simulation.on("tick", tickActions());
-    // var link_force = d3.forceLink(link_data).id(function (d) {
-    //     return d.name;
-    // });
-    // simulation.force("links", link_force);
-    // var link = svg.append("g")
-    //     .attr("class", "links")
-    //     .selectAll("line")
-    //     .data(link_data)
-    //     .enter()
-    //     .append("line")
-    //     .attr("stroke-width", 2);
-    //
-    // function tickActions() {
-    //     node
-    //         .attr("cx", function (d) { return d.x})
-    //         .attr("cy", function (d) { return d.y});
-    //     link
-    //         .attr("x1", function (d) { return d.source.x})
-    //         .attr("y1", function (d) { return d.source.y})
-    //         .attr("x2", function (d) { return d.target.x})
-    //         .attr("y2", function (d) { return d.target.y});
-    // }
 }
 
 function circleColor(d) {
